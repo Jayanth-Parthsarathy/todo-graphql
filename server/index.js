96ea793865106e57ebd267808e76f9ea04df9f9b
jayanth-parthsarathy <jparthaa@gmail.com>
2023-06-24 10:43:08 +0530

    Create backend and connect to db

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const cors = require('cors')
+const { graphqlHTTP } = require('express-graphql')
+const connectDB = require('./config/db')
+
+
+
+
+const app = express()
+require('dotenv').config()
+const port = process.env.PORT || 3000
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+connectDB()
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    graphiql: true,
+  })
+)
+
+app.listen(port, () => {
+  console.log(`Backend server running on port: ${port}`)
+})
